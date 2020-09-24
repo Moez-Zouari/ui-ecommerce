f97aa82672351da3a9075c0293ea7fd16b2d0cfe
Moez-Zouari <moez_zouari@hotmail.fr>
2020-09-24 15:34:13 +0100

    Create new component for menu/ Enhance menu component to road data from product service / Update product service to all URL on Spring boot app

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -9,7 +10,10 @@ import { map } from 'rxjs/operators';
 })
 export class ProductService {
 
+
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+  
 
   constructor(private  httpClient : HttpClient) { }
 
@@ -18,14 +22,28 @@ export class ProductService {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     // Update this for search URL
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded : {
     products : Product[];
+  }
+}
+
+
+interface GetResponseProductCategory {
+  _embedded : {
+    productCategory : ProductCategory[];
   }
 }
