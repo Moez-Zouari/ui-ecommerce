bdf8449f07363bf3d8d7b3fa98dc5f6946875afc
Moez-Zouari <moez_zouari@hotmail.fr>
2020-10-03 16:14:51 +0100

    Update ProductListComponent to handle pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -10,20 +10,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
-  currentCategoryId: number;
-  searchMode: boolean;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
   currentCategoryName : string ;
+
+  // New propreties for pagination
+  thePageNumber : number = 1 ;
+  thePageSize : number = 10 ;
+  theTotalElements : number = 0 ; 
+  
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
 
+
+//---------------------------------------------------------------------------------
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
+//---------------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------------
   listProducts() {
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
@@ -33,11 +44,11 @@ export class ProductListComponent implements OnInit {
     else {
       this.handleListProducts();
     }
-
   }
+//---------------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------------
   handleSearchProducts() {
-
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
     // now search for the products using keyword
@@ -47,7 +58,10 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
+//---------------------------------------------------------------------------------
 
+
+//---------------------------------------------------------------------------------
   handleListProducts() {
 
     // check if "id" parameter is available
@@ -65,11 +79,33 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryName = 'Books';
     }
 
+    //
+    // Check if we have a different category than previous
+    //  Note : Angular will reuse a component if it is currently being viewed
+
+    // If we have a different category id than previous
+    // Then set thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1 ;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )    
+    this.productService.getProductListPaginate(this.thePageNumber -1,
+                                                this.thePageSize,
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult()); 
   }
+//---------------------------------------------------------------------------------
+  processResult() {
+    return data => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number +1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.theTotalElements;
+    }
+  }
+
 }
