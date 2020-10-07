641285d10d4f08ea4ea7109c17cc7913275bcb3a
Zouari <moez_zouari@hotmail.fr>
2020-10-07 16:48:44 +0100

    Develop Add to Cart ine product details component

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { CartItem } from  'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
+  
 })
 export class ProductDetailsComponent implements OnInit {
 
   product : Product = new Product() ;
   constructor(private productService : ProductService,
+              private cartService : CartService,
               private route : ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -31,5 +35,14 @@ export class ProductDetailsComponent implements OnInit {
       }
     )
   }
+
+  addToCart(){
+    console.log(`Adding to cart:${this.product.name},${this.product.unitPrice}`);
+   
+    const theCartItem = new CartItem(this.product);
+
+    this.cartService.addToCart(theCartItem);
+  }
+
 
 }
