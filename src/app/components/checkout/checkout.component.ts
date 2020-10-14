a5259227274b317aae6a79c283508e2faf79842c
Zouari <moez_zouari@hotmail.fr>
2020-10-14 19:45:57 +0100

    Credit card validators form control

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -94,10 +94,13 @@ export class CheckoutComponent implements OnInit {
       }),
 
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl ('',[Validators.required]),
+        nameOnCard: new FormControl ('',
+                                    [Validators.required,
+                                    Validators.minLength(2),
+                                    Luv2ShopValidators.notOnlyWhitespace]),
+        cardNumber: new FormControl ('',[Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl ('',[Validators.required, Validators.pattern('[0-9]{3}')]),
         expirationMonth: [''],
         expirationYear: ['']
       })
@@ -165,6 +168,14 @@ export class CheckoutComponent implements OnInit {
    get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
    get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
    get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+
+     //-----------------Getter methods to access to creditCard form control-------------------------
+     get creditCardType() { return this.checkoutFormGroup.get('creditCard.cardType'); }
+     get creditCardNameOnCard() { return this.checkoutFormGroup.get('creditCard.nameOnCard'); }
+     get creditCardNumber() { return this.checkoutFormGroup.get('creditCard.cardNumber'); }
+     get creditCardSecurityCode() { return this.checkoutFormGroup.get('creditCard.securityCode'); }
+     
+
 
      //-----------------Getter methods to access to billingAddress form control-------------------------
      get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
