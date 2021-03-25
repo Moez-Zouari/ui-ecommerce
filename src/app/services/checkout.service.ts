803a5f6035f3d81dc17e288d07da613c305db66e
Moez <moez_zouari@hotmail.fr>
2021-03-25 16:20:23 +0100

    save the order - create checkoutService

---
+++ src/app/services/checkout.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { Purchase } from './../common/purchase';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
+
+  constructor(private httpClient : HttpClient) { }
+
+  placeOrder(purchase: Purchase): Observable<any> {
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+}
